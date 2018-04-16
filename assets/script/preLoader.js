6da271d1885cb0d5056740965157e24eeabcd535
RisingGeek <bhavaypuri15@gmail.com>
2018-04-16 23:37:41 +0530

    Added Pre-Loader

---
+++ assets/script/preLoader.js
@@ -0,0 +1,5 @@
+setTimeout(loadContent, 2000);
+function loadContent() {
+    document.getElementById('load').style.display='block';
+    document.getElementById('pre-loader').style.display='none';
+}
